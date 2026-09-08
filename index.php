6fb1c92867741f1aa7fc4c3a744131d98acaa6e2
Ayuk Amelia <ameliafertilizer123@gmail.com>
2026-09-08 23:03:39 +0100

    mini ineventory

--- index.php
+++ index.php
@@ -1,64 +1,77 @@
-// Creating a form
 
+
+
+<?php
+session_start();
+?>
+<h1>Mini Inventory</h1>
 <form action="#" method="POST">
-    <input type="text" name="car">
-    <input type="text" name="price">
-    <input type="text" name="quantity">
-    <button>submit</button>
+<label>product</label>
+<input type="text" name="product_name"> 
+<br><br>
+<label>price</label>
+<input type="text" name="price"> 
+<br><br>
+<label>quantity</label>
+<input type="text" name="quantity"> 
+<br><br>
+<label>search a product </label>
+<input type="text" name="search"> 
+<br><br>
+
+<button>submit</button> <br>
+
+
 </form>
 
 <?php
+//validate form inputs
+if(isset($_POST["product_name"])){
+    if(!empty($_POST["product_name"]) && !empty($_POST["price"]) && $_POST['price']>0 && !empty($_POST['quantity'])&& $_POST['quantity']>0 ){
 
-// Stores products in an array, counts them, and displays the number of products
-$product = ['laptops', 'mouse', 'touch pen'];
-echo count($product) . '<br>';
+//calculate the total
+$total = $_POST['price']*$_POST['quantity'];
 
-// Checks if the form was submitted and validates whether the input fields are empty
-if (isset($_POST['car'])) {
+echo $total .'<br>';
+//Adding product to the inventory
+$products = ['phone','laptop','mouse'];
 
-    if (empty($_POST['car'])) {
-        echo 'car required' . '<br>';
-    }
 
-    if (empty($_POST['price'])) {
-        echo 'price required' . '<br>';
-    }
+if (in_array($_POST['product_name'],$products)) {
+    echo 'product exist'.'<br>';
+}
+else{
+    echo 'product added' .'<br>';
 
-    if (empty($_POST['quantity'])) {
-        echo 'quantity required' . '<br>';
-    }
+array_push( $products,$_POST['product_name']);
+ // saving product information to the file
+    file_put_contents('stock.txt', $_POST['product_name'] . ' ' . $_POST['price'] . ' ' . $_POST['quantity'] . ' '. $total . PHP_EOL, FILE_APPEND);
 
-    if ($_POST['price'] > 0 && $_POST['quantity'] > 0) {
-        $total = $_POST['price'] * $_POST['quantity'];
-        echo $total . '<br>';
-    }
+$contents = file_get_contents('stock.txt');
+
+echo $contents .'<br>';
+}
+foreach ($products as  $product) {
+    echo  $product .'<br>';
+}
+// searching a product
+if (empty($_POST['search'])) {
+    echo 'search is empty'.'<br>';
 }
 
-// Function that calculates the total using price and quantity
-// and displays the result when the function is called
-function calculatePrice($price, $quantity) {
-    $result = $price * $quantity;
-    echo $result . '<br>';
+elseif(in_array($_POST['search'],$products)) {
+    echo 'product found'.'<br>';
+   
 }
-
-calculatePrice(100, 10);
-
-// Starts the session and stores a username in the session
-session_start();
+else{
+    echo 'product not found'.'<br>';
+}
+// starting user sessions
 $_SESSION['username'] = 'Amelia';
-
-// Checks if the username exists in the session
-if (isset($_SESSION['username'])) {
-    echo $_SESSION['username'] . '<br>';
-} else {
-    echo 'username does not exist' . '<br>';
+echo $_SESSION['username'] .'<br>';
+    }
+    else {
+    echo 'Please enter valid product information.';
 }
-
-// Checks if the product file exists and reads its contents
-if (file_exists('product.txt')) {
-    echo file_get_contents('product.txt') . '<br>';
-} else {
-    echo 'file not found' . '<br>';
 }
-
 ?>
